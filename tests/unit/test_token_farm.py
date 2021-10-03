8ea3737369909994de4759f9abe8c24f6e518092
ermz <eramirez4369@gmail.com>
2021-10-02 23:58:09 -0400

    Fixed previous test and added testing for issuing tokens

--- tests/unit/test_token_farm.py
+++ tests/unit/test_token_farm.py
@@ -1,8 +1,9 @@
 import pytest
 import brownie
 from brownie import network, StakeToken, TokenFarm
-from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account, get_contract
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, INITIAL_VALUE, get_account, get_contract
 from scripts.deploy import deploy_token_farm_and_dapp_token
+from web3 import Web3
 
 def test_set_price_feed_contract():
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
@@ -23,19 +24,24 @@ def test_stake_tokens(amount_staked):
     dapp_token.approve(token_farm.address, amount_staked, {"from": account})
     assert token_farm.uniqueTokensStaked(account) == 0
     assert token_farm.stakingBalance(dapp_token.address, account) == 0
-    token_farm.stakeTokens(1, dapp_token.address, {"from": account})
+    token_farm.stakeTokens(amount_staked, dapp_token.address, {"from": account})
     assert token_farm.uniqueTokensStaked(account) == 1
-    assert token_farm.stakingBalance(dapp_token.address, account) > 0
+    assert token_farm.stakingBalance(dapp_token.address, account) == amount_staked
+    assert token_farm.stakers(0) == account
+    return token_farm, dapp_token
 
 def test_issue_tokens(amount_staked):
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         pytest.skip("Only for local testing!")
     account = get_account()
     non_owner = get_account(index=1)
-    token_farm, dapp_token = deploy_token_farm_and_dapp_token()
-    dapp_token.approve(token_farm.address, amount_staked, {"from": account})
-    token_farm.stakeTokens(1, dapp_token.address, {"from": account})
+    token_farm, dapp_token = test_stake_tokens(amount_staked)
+    starting_balance = dapp_token.balanceOf(account)
     with brownie.reverts():
         token_farm.issueTokens({"from": non_owner})
     token_farm.issueTokens({"from": account})
-    
+    assert Web3.toWei(2099, "ether") == (starting_balance + INITIAL_VALUE)
+    assert (
+        dapp_token.balanceOf(account)
+        == starting_balance + INITIAL_VALUE
+    )
